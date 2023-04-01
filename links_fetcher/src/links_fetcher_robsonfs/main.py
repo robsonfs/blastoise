a0f2d30d1a59ff918ee31e388f1a5f8945158d3d
Robson Santos <robfds@gmail.com>
2023-04-01 13:45:56 -0300

    feat(Python package for link extraction): get HTML code from provided URL

--- links_fetcher/src/links_fetcher_robsonfs/main.py
+++ links_fetcher/src/links_fetcher_robsonfs/main.py
@@ -1,4 +1,9 @@
 import requests
 
 def get_source(url: str) -> (int, str):
-    pass
+    try:
+        resp = requests.get(url)
+    except Exception as err:
+        raise Exception("Could not fetch the provided URL")
+
+    return resp.status_code, resp.text
