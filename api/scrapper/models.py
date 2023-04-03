eefbd985adf9f969f3b85f814972ec0de213e177
Robson Santos <robfds@gmail.com>
2023-04-03 01:31:25 -0300

    feat(Backend Side): add field links to page

--- api/scrapper/models.py
+++ api/scrapper/models.py
@@ -16,6 +16,7 @@ class Page(models.Model):
     url = models.URLField(max_length=200)
     title = models.CharField(max_length=150, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    links = models.JSONField(blank=True, null=True)
     status = models.CharField(
         max_length=11, choices=STATUS_CHOICES, default="in_progress"
     )
@@ -26,7 +27,7 @@ class Page(models.Model):
 
     @property
     def total_links(self):
-        return 0
+        return len(self.links)
 
     def __str__(self):
         return self.title or self.url
