d974e836629585796b07e3055426fc714a8720b2
Robson Santos <robfds@gmail.com>
2023-04-03 01:08:09 -0300

    feat(Backend Side): create page capability added

--- api/scrapper/views.py
+++ api/scrapper/views.py
@@ -13,3 +13,16 @@ class PageListApiView(APIView):
         serializer = PageSerializer(pages, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            "url": request.data.get("url")
+        }
+
+        serializer = PageSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
