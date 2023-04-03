1650a8073a410653be4848ee07e2a9df02b6f587
Robson Santos <robfds@gmail.com>
2023-04-02 21:20:20 -0300

    feat(Backend Side): add models for pages and links

--- api/scrapper/models.py
+++ api/scrapper/models.py
@@ -1,3 +1,43 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Page(models.Model):
+
+    STATUS_CHOICES = (
+        ("in_progress", "In progess..."),
+        ("done", "Done"),
+    )
+
+    uuid = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+    url = models.URLField(max_length=200)
+    title = models.CharField(max_length=150, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=11, choices=STATUS_CHOICES, default="in_progress"
+    )
+
+    @property
+    def name(self):
+        return self.title or "page processing"
+
+    def __str__(self):
+        return self.name
+
+
+class PageLink(models.Model):
+    uuid = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+    href_attr = models.URLField(max_length=200)
+    name = models.CharField(max_length=50)
+    page = models.ForeignKey(
+        "scrapper.Page", on_delete=models.CASCADE, related_name="links"
+    )
+
+    def __str__(self):
+        return self.name
+
